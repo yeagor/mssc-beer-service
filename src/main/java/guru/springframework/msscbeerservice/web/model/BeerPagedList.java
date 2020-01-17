1f7756a14f6d3c09f25508a326957fdbd9c89ce3
Georgy Vashchenko <gvashchenko@lineate.com>
2020-01-18 00:19:59 +0600

    Add JsonCreator for paged object. Close #11

--- src/main/java/guru/springframework/msscbeerservice/web/model/BeerPagedList.java
+++ src/main/java/guru/springframework/msscbeerservice/web/model/BeerPagedList.java
@@ -1,6 +1,9 @@
 package guru.springframework.msscbeerservice.web.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
 import java.util.List;
@@ -12,5 +15,18 @@ public class BeerPagedList extends PageImpl<BeerDto> {
 
     public BeerPagedList(List<BeerDto> content) {
         super(content);
+    }
+
+    public BeerPagedList(@JsonProperty("content") List<BeerDto> content,
+                         @JsonProperty("number") int number,
+                         @JsonProperty("size") int size,
+                         @JsonProperty("totalElements") Long totalElements,
+                         @JsonProperty("pageable") JsonNode pageable,
+                         @JsonProperty("last") boolean last,
+                         @JsonProperty("totalPages") int totalPages,
+                         @JsonProperty("sort") JsonNode sort,
+                         @JsonProperty("first") boolean first,
+                         @JsonProperty("numberOfElements") int numberOfElements) {
+        super(content, PageRequest.of(number, size), totalElements);
     }
 }
